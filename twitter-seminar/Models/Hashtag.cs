860fcb0a20ccb5c6e4de98eec478a0adbe184b33
Josipa Braica <josipa@dump.hr>
2015-12-03 19:14:26 +0100

    Added DAL folder

--- twitter-seminar/Models/Hashtag.cs
+++ twitter-seminar/Models/Hashtag.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace twitter_seminar.Models
     public class Hashtag
     {
         public int ID { get; set; }
+        [RegularExpression(@"(?:^|\s)\s*(#[A-Za-z][A-Za-z0-9-_]+)")]
+        //[RegularExpression(@"(?<=\s|^)#(\w*[A-Za-z_]+\w*)")]
         public string Text { get; set; }
 
         public virtual Tweet Tweet { get; set; }
