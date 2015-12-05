400a8e22ee82fff0c6e3544422817e56c80191b7
Josipa Braica <josipa@dump.hr>
2015-12-05 19:55:58 +0100

    Change index.html and partially add DAL files

---
+++ twitter-seminar/DAL/TwitterContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using twitter_seminar.Models;
+
+namespace twitter_seminar.DAL
+{
+    public class TwitterContext : DbContext
+    {
+        public DbSet<Tweet> Tweets { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Hashtag> Hashtags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ twitter-seminar/DAL/TwitterInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace twitter_seminar.DAL
+{
+    public class TwitterInitializer : DropCreateDatabaseAlways<TwitterContext>
+    {
+        protected override void Seed(TwitterContext context)
+        {
+            
+        }
+    }
+}
